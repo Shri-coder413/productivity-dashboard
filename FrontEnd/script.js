f238cd6214db0f86c0838ec7070294e11dc1f40f
Shri-coder413 <shrinivasnakadi@gmail.com>
2026-03-05 10:44:00 +0530

    change theme modification

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -574,41 +574,14 @@ function changeTheme() {
   const themeBtn = document.querySelector(".theme-toggle");
 
   const themes = [
-    {
-      pri: "#FFF7CD",
-      sec: "#FDC3A1",
-      tri1: "#FB9B8F",
-      tri2: "#b35b72",
-    },
-    {
-      pri: "#F7F8F0",
-      sec: "#9CD5FF",
-      tri1: "#7AAACE",
-      tri2: "#355872",
-    },
-    {
-      pri: "#FFFDF1",
-      sec: "#FFCE99",
-      tri1: "#FF9644",
-      tri2: "#562F00",
-    },
-    {
-      pri: "#F7F7F7",
-      sec: "#777c8b",
-      tri1: "#393E46",
-      tri2: "#222831",
-    },
+    { pri: "#FFF7CD", sec: "#FDC3A1", tri1: "#FB9B8F", tri2: "#b35b72" },
+    { pri: "#F7F8F0", sec: "#9CD5FF", tri1: "#7AAACE", tri2: "#355872" },
+    { pri: "#FFFDF1", sec: "#FFCE99", tri1: "#FF9644", tri2: "#562F00" },
+    { pri: "#F7F7F7", sec: "#777c8b", tri1: "#393E46", tri2: "#222831" },
   ];
 
   let currentIndex = parseInt(localStorage.getItem("themeIndex")) || 0;
 
-  themeBtn.addEventListener("click", function () {
-    currentIndex = (1 + currentIndex) % themes.length;
-
-    applyTheme(currentIndex);
-
-    localStorage.setItem("themeIndex", currentIndex);
-  });
   function applyTheme(index) {
     const rootElement = document.documentElement;
 
@@ -617,6 +590,16 @@ function changeTheme() {
     rootElement.style.setProperty("--tri1", themes[index].tri1);
     rootElement.style.setProperty("--tri2", themes[index].tri2);
   }
+
+  applyTheme(currentIndex);
+
+  themeBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % themes.length;
+
+    applyTheme(currentIndex);
+
+    localStorage.setItem("themeIndex", currentIndex);
+  });
 }
 
 openSideBar();
